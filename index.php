e4d5441643629a15f550deac8f830497f5187cd4
Itsatsu <e.gigondan@gmail.com>
2018-12-04 18:58:23 +0100

    'monloup'

--- index.php
+++ index.php
@@ -3,17 +3,48 @@
 class Person{
 	private $prenom;
 	private $nom;
+	private $parole;
+	private $monloup;
 	public function setPrenom($name){
 		$this->prenom = $name;
 	}
-	public function getPrenom($name){
-		
+	public function getPrenom(){
+		return $this->prenom;
+	}
+	public function speak($speak){
+		return ($this->parole = $speak).',je suis '.$this->prenom.'mon loup se nomme '.$this->monloup->getNom();
+	}
+	public function __construct($nom, $prenom){
+		$this->nom = $nom;
+		$this->prenom = $prenom;
+	}
+	public function buy($monloup){
+		$this->monloup = $monloup;
 	}
 }
+class loup{
+	private $NOM1;
 
+	public function setNom($NOM2){
+		$this->NOM1 = $NOM2;
+	}
+	public function getNom(){
+		return $this->NOM1;
+	}
+	public function __construct($NOM1){
+	$this->NOM1 = $NOM1; 
 
-$eric = new Person;
-$eric->setPrenom('eric');
+}
+}
+$gerald = new loup('gerald');
+//$gerald->setNOM('gerald');
+//var_dump($gerald->getNom());
+var_dump($gerald);
+
+$eric = new Person("Gigondan", "Eric");
+$eric->buy($gerald);
+//$eric->setPrenom('eric');
 var_dump($eric);
-
+//var_dump($eric->getPrenom());
+var_dump($eric->speak('bonjour je suis jeune'));
  ?>
